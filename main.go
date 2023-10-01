deeeae79ed123461481578c3778aefb034a088a8
gastraderr <gaslimits@gmail.com>
2023-09-30 23:54:04 -0400

    middleware fix

--- main.go
+++ main.go
@@ -15,16 +15,8 @@ import (
 )
 
 func main() {
-	r := chi.NewRouter()
-
-	tpl := views.Must(views.ParseFS(templates.FS, "home.html", "tailwind.html"))
-	r.Get("/", controllers.StaticHandler(tpl))
-
-	tpl = views.Must(views.ParseFS(templates.FS, "contact.html", "tailwind.html"))
-	r.Get("/contact", controllers.FAQ(tpl))
-
+	//setup the db
 	cfg := models.DefaultPostgresConfig()
-	//host=localhost port=4321 user=postgres password=admin123 dbname=website sslmode=disable
 	db, err := models.Open(cfg)
 	if err != nil {
 		panic(err)
@@ -32,10 +24,11 @@ func main() {
 	defer db.Close()
 
 	err = models.MigrateFS(db, migrations.FS, ".")
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 
+	//setup services
 	userService := models.UserService{
 		DB: db,
 	}
@@ -43,17 +36,34 @@ func main() {
 		DB: db,
 	}
 
-	usersC := controllers.Users{
-		UserService: &userService, //TODO = set this
+	//setup middleware
+	umw := controllers.UserMiddleware{
 		SessionService: &sessionService,
 	}
-	//TEMPLATE PARSING
+	csrfKey := "u2312casdyug682yubbcjyuihyu3bnsx"
+	csrfMw := csrf.Protect([]byte(csrfKey), csrf.Secure(false))
+
+	//setup controllers
+	usersC := controllers.Users{
+		UserService:    &userService, //TODO = set this
+		SessionService: &sessionService,
+	}
 	usersC.Templates.New = views.Must(views.ParseFS(
 		templates.FS,
 		"signup.html", "tailwind.html"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(
 		templates.FS,
 		"signin.html", "tailwind.html"))
+
+	//setup router and routes
+	r := chi.NewRouter()
+	r.Use(csrfMw)
+	r.Use(umw.SetUser)
+	tpl := views.Must(views.ParseFS(templates.FS, "home.html", "tailwind.html"))
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.html", "tailwind.html"))
+	r.Get("/contact", controllers.FAQ(tpl))
 
 	//ROUTE HANDLING
 	r.Get("/signup", usersC.New)
@@ -62,20 +72,15 @@ func main() {
 	r.Post("/signin", usersC.ProcessSignIn)
 	r.Post("/signout", usersC.ProcessSignOut)
 	r.Get("/users/me", usersC.CurrentUser)
-
-
+	r.Route("/users/me", func(r chi.Router){
+		r.Use(umw.RequireUser)
+		r.Get("/", usersC.CurrentUser)
+	})
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	})
 
-	umw := controllers.UserMiddleware{
-		SessionService: &sessionService,
-	}
-
-	csrfKey := "u2312casdyug682yubbcjyuihyu3bnsx"
-	csrfMw := csrf.Protect([]byte(csrfKey), csrf.Secure(false))
-
 	fmt.Println("starting the server on :3000..")
-	http.ListenAndServe("127.0.0.1:3000", csrfMw(umw.SetUser(r)))
+	http.ListenAndServe("127.0.0.1:3000", r)
 
 }
