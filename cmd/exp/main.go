9e3ae5ae146c48e0cd55cb559a64acd413684eae
gastraderr <gaslimits@gmail.com>
2023-10-02 13:16:26 -0400

    env variables

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -2,26 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	"github.com/gastrader/website/models"
+	"github.com/joho/godotenv"
 )
 
-
-const (
-	host     = "sandbox.smtp.mailtrap.io"
-	port     = 25
-	username = "297bd7f4a812a3"
-	password = "b25328c4118822"
-)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal("Error loading .env")
+	}
+	host := os.Getenv("SMTP_HOST")
+	portStr := os.Getenv("SMTP_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil{
+		panic(err)
+	}
+	username := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
 	es := models.NewEmailService(models.SMTPConfig{
 		Host:     host,
 		Port:     port,
 		Username: username,
 		Password: password,
 	})
-	err := es.ForgotPassword("gavinp96@gmail.com", "https://localdankness.com")
+	err = es.ForgotPassword("gavinp96@gmail.com", "https://localhost.com")
 	if err != nil {
 		panic(err)
 
