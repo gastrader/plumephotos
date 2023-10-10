ccdc297f7f1f0dd760e31562abfe85d1cb23ce27
gastraderr <gaslimits@gmail.com>
2023-10-09 21:50:20 -0400

    major design changes

--- controllers/users.go
+++ controllers/users.go
@@ -12,16 +12,16 @@ import (
 
 type Users struct {
 	Templates struct {
-		New    Template
-		SignIn Template
+		New            Template
+		SignIn         Template
 		ForgotPassword Template
 		CheckYourEmail Template
-		ResetPassword Template
+		ResetPassword  Template
 	}
-	UserService    *models.UserService
-	SessionService *models.SessionService
+	UserService          *models.UserService
+	SessionService       *models.SessionService
 	PasswordResetService *models.PasswordResetService
-	EmailService *models.EmailService
+	EmailService         *models.EmailService
 }
 
 type UserData struct {
@@ -30,6 +30,12 @@ type UserData struct {
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	user := context.User(ctx)
+	if user != nil {
+		http.Redirect(w, r, "/galleries", http.StatusSeeOther)
+		return
+	}
 	var data struct {
 		Email string
 	}
@@ -39,7 +45,7 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 	var data struct {
-		Email string
+		Email    string
 		Password string
 	}
 	data.Email = r.FormValue("email")
@@ -47,7 +53,7 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 
 	user, err := u.UserService.Create(data.Email, data.Password)
 	if err != nil {
-		if errors.Is(err, models.ErrEmailTaken){
+		if errors.Is(err, models.ErrEmailTaken) {
 			err = errors.Public(err, "That email address has been taken.")
 		}
 		u.Templates.New.Execute(w, r, data, err)
@@ -66,6 +72,12 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	user := context.User(ctx)
+	if user != nil {
+		http.Redirect(w, r, "/galleries", http.StatusSeeOther)
+		return
+	}
 	var data struct {
 		Email string
 	}
@@ -82,12 +94,12 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 	data.Password = r.FormValue("password")
 	user, err := u.UserService.Authenticate(data.Email, data.Password)
 	if err != nil {
-		if errors.Is(err, models.ErrLoginNotFound){
+		if errors.Is(err, models.ErrLoginNotFound) {
 			err = errors.Public(err, "The email address and password do not match.")
 			u.Templates.SignIn.Execute(w, r, data, err)
 			return
 		}
-		if errors.Is(err, models.ErrEmailNotFound){
+		if errors.Is(err, models.ErrEmailNotFound) {
 			err = errors.Public(err, "The email address was not found.")
 			u.Templates.SignIn.Execute(w, r, data, err)
 			return
@@ -134,8 +146,8 @@ type UserMiddleware struct {
 	SessionService *models.SessionService
 }
 
-//allows us to have URL like mysite.com/forgot-pw?email=jon@jon.com
-func (u Users) ForgotPassword(w http.ResponseWriter, r *http.Request){
+// allows us to have URL like mysite.com/forgot-pw?email=jon@jon.com
+func (u Users) ForgotPassword(w http.ResponseWriter, r *http.Request) {
 	var data struct {
 		Email string
 	}
@@ -149,7 +161,7 @@ func (u Users) ProcessForgotPassword(w http.ResponseWriter, r *http.Request) {
 	}
 	data.Email = r.FormValue("email")
 	pwReset, err := u.PasswordResetService.Create(data.Email)
-	if err != nil{
+	if err != nil {
 		//handle other cases if user DNE w/ that email address
 		fmt.Println(err)
 		http.Error(w, "Something went wrong..", http.StatusInternalServerError)
@@ -158,9 +170,9 @@ func (u Users) ProcessForgotPassword(w http.ResponseWriter, r *http.Request) {
 	vals := url.Values{
 		"token": {pwReset.Token},
 	}
-	resetURL := "https://www.plumephotos.com/reset-pw?"+ vals.Encode()
+	resetURL := "https://www.plumephotos.com/reset-pw?" + vals.Encode()
 	err = u.EmailService.ForgotPassword(data.Email, resetURL)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "Something went wrong..", http.StatusInternalServerError)
 		return
@@ -170,7 +182,7 @@ func (u Users) ProcessForgotPassword(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (u Users) ResetPassword(w http.ResponseWriter, r *http.Request){
+func (u Users) ResetPassword(w http.ResponseWriter, r *http.Request) {
 	var data struct {
 		Token string
 	}
@@ -178,23 +190,23 @@ func (u Users) ResetPassword(w http.ResponseWriter, r *http.Request){
 	u.Templates.ResetPassword.Execute(w, r, data)
 }
 
-func (u Users) ProcessResetPassword(w http.ResponseWriter, r *http.Request){
+func (u Users) ProcessResetPassword(w http.ResponseWriter, r *http.Request) {
 	var data struct {
-		Token string
+		Token    string
 		Password string
 	}
 	data.Token = r.FormValue("token")
 	data.Password = r.FormValue("password")
-	
+
 	user, err := u.PasswordResetService.Consume(data.Token)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
 	//Update user's password
 	err = u.UserService.UpdatePassword(user.ID, data.Password)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		return
@@ -202,7 +214,7 @@ func (u Users) ProcessResetPassword(w http.ResponseWriter, r *http.Request){
 
 	//Sign user in after pw reset. Same as "process sign in"
 	session, err := u.SessionService.Create(user.ID)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		http.Redirect(w, r, "/signin", http.StatusFound)
 		return
@@ -210,7 +222,6 @@ func (u Users) ProcessResetPassword(w http.ResponseWriter, r *http.Request){
 	setCookie(w, CookieSession, session.Token)
 	http.Redirect(w, r, "/galleries", http.StatusFound)
 }
-
 
 func (umw UserMiddleware) SetUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -232,7 +243,7 @@ func (umw UserMiddleware) SetUser(next http.Handler) http.Handler {
 }
 
 func (umw UserMiddleware) RequireUser(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := context.User(r.Context())
 		if user == nil {
 			http.Redirect(w, r, "/signin", http.StatusFound)
@@ -241,4 +252,3 @@ func (umw UserMiddleware) RequireUser(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
-
